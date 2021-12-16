47704c39d8b468d0b790cda64b25da4125f47757
azimny86 <azimny86@gmail.com>
2021-12-16 19:31:13 +0000

    Adding Ragistration

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
 
-mongo = PyMongo((app))
+mongo = PyMongo(app)
 
 @app.route("/")
 @app.route("/recipes")
@@ -23,6 +23,24 @@ def recipes():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+# check if username already in Database
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+# put new user into 'session' cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful")
+        return redirect(url_for("profile", username=session["user"]))
+
     return render_template("register.html")
 
 if __name__ == "__main__":
